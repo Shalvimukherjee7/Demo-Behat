e527cd0ea2528a1b8f4d9e7df10b0c19942c136a
Shalvi Mukherjee <76940025+Shalvimukherjee7@users.noreply.github.com>
2021-03-22 18:39:53 +0530

    context file

---
+++ features/bootstrap/CartContext.php
@@ -0,0 +1,70 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Selector\CssSelector;
+use PHPUnit\Framework\Assert;
+
+/**
+ * Defines application features from the specific context.
+ */
+class CartContext extends \Drupal\DrupalExtension\Context\RawDrupalContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Given /^I click on add to cart "([^"]*)"$/
+     */
+    public function addToCart($index)
+    {
+       $this->getSession()
+         ->getPage()
+         ->find('xpath', "//div[@id='content']//div[$index]/div[1]/div[3]/button[1]")
+         ->click();
+        
+    }
+
+    /**
+     * @Given /^I open cart popup$/
+     */
+    public function cartTotalButton()
+    {
+       $this->getSession()
+         ->getPage()
+         ->find('xpath', "//span[@id='cart-total']")
+         ->click();
+    }
+
+    /**
+     * @Given /^I should see total "([^"]*)" products in cart popup$/
+     */
+    public function cartTotalCount($index)
+    {
+       $this->assertSession()->elementTextContains('xpath', "//span[@id='cart-total']",$index ." item(s)");
+    }
+
+    /** @AfterScenario @addtocart */
+    public function after()
+    {
+      $elements=$this->getSession()
+         ->getPage()
+         ->findAll('css',"[title='Remove']");
+      foreach($elements as $value){
+      $value=$this->getSession()
+         ->getPage()
+         ->find('xpath',"//div[@id='cart']//tr[1]//button")
+         ->click();
+    
+        }
+    }
+}
